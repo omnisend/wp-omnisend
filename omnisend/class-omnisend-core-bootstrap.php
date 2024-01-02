f3c27ee452fef50ee56fd4ce97dbdd72246c764a
Nerijus Zaniauskas <nerijus.z@omnisend.com>
2024-01-02 15:38:27 +0200

    Extract connection

--- omnisend/class-omnisend-core-bootstrap.php
+++ omnisend/class-omnisend-core-bootstrap.php
@@ -22,6 +22,8 @@ defined( 'ABSPATH' ) || exit;
 
 define( 'OMNISEND_CORE_SETTINGS_PAGE', 'omnisend' );
 
+require_once 'module/class-omnisend-core-connection.php';
+
 add_action( 'admin_menu', 'Omnisend_Core_Bootstrap::add_admin_menu' );
 
 class Omnisend_Core_Bootstrap {
@@ -31,9 +33,7 @@ class Omnisend_Core_Bootstrap {
 		$menu_title    = 'Omnisend Core';
 		$capability    = 'manage_options';
 		$menu_slug     = OMNISEND_CORE_SETTINGS_PAGE;
-		$function      = function () {
-			echo 'It works';
-		};
+		$function      = 'Omnisend_Core_Connection::display';
 		$omnisend_icon = 'data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMzIgMzIiIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0yNi40NDg1IDBINS41NDAyQzIuNDgyMzcgMCAwIDIuNDgxNDkgMCA1LjUzODI0VjI2LjQzOTJDMCAyOS40OTU5IDIuNDgyMzcgMzEuOTc3NCA1LjU0MDIgMzEuOTc3NEg3Ljg3NTg4SDE1LjA2MzVWMjcuMzk4QzE1LjA2MzUgMjIuOTg3NyAxNC41MjE5IDE5LjU5MjUgMTEuMjM4NCAxOC42MjI1VjE1LjAxM0MxNi41NzU1IDE1LjQ4NjggMTkuMjI3MSAxOS40NTcyIDE5LjIyNzEgMjUuOTg4VjMySDIzLjU3MTJIMjYuNDU5OEMyOS41MTc2IDMyIDMyIDI5LjUxODUgMzIgMjYuNDYxOFY1LjUzODI0QzMxLjk4ODcgMi40ODE0OSAyOS41MDYzIDAgMjYuNDQ4NSAwWk0xNi4xNTggMTEuNTUwMkMxNC40NjU0IDExLjU1MDIgMTMuMDg4OSAxMC4xNzQxIDEzLjA4ODkgOC40ODIyQzEzLjA4ODkgNi43OTAyNyAxNC40NjU0IDUuNDE0MTcgMTYuMTU4IDUuNDE0MTdDMTcuODUwNSA1LjQxNDE3IDE5LjIxNTggNi43OTAyNyAxOS4yMTU4IDguNDkzNDhDMTkuMjE1OCAxMC4xODU0IDE3Ljg1MDUgMTEuNTUwMiAxNi4xNTggMTEuNTUwMloiCiAgICAgICAgICBmaWxsPSJ3aGl0ZSIvPgo8L3N2Zz4K';
 		$position      = 2;
 
---
+++ omnisend/module/class-omnisend-core-connection.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Omnisend plugin
+ *
+ * @package OmnisendPlugin
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+class Omnisend_Core_Connection {
+
+	public static function display() {
+		echo 'It works!';
+	}
+}
