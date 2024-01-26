14cd534902fc0eca86477325a3b298dc416895e4
Nerijus Zaniauskas <nerijus.z@omnisend.com>
2024-01-26 17:23:40 +0200

    Bug fix

--- omnisend/includes/Internal/class-snippet.php
+++ omnisend/includes/Internal/class-snippet.php
@@ -12,9 +12,9 @@ defined( 'ABSPATH' ) || die( 'no direct access' );
 class Snippet {
 
 	public static function add() {
-		$brand_id = Omnisend_Core_Options::get_brand_id();
+		$brand_id = Options::get_brand_id();
 		if ( $brand_id ) {
-			require_once 'view/snippet.html';
+			require_once __DIR__ . '/../../view/snippet.html';
 		}
 	}
 }
