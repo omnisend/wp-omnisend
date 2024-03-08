37116eb0b9ee05a79046772c411222a799c77a93
gabiermi <gabriele@omnisend.com>
2024-03-08 20:35:49 +0800

    remove unused code

--- omnisend/includes/Internal/class-connection.php
+++ omnisend/includes/Internal/class-connection.php
@@ -27,11 +27,6 @@ class Connection {
 			$brand_id = ! empty( $response['brandID'] ) ? $response['brandID'] : '';
 
 			if ( ! $brand_id ) {
-				require_once __DIR__ . '/../../view/landing-page.html';
-				return;
-			}
-
-			if ( ! $brand_id ) {
 				echo '<div class="notice notice-error"><p>The connection didn’t go through. Check if the API key is correct.</p></div>';
 				require_once __DIR__ . '/../../view/connection-form.html';
 				return;
